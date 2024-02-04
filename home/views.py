f8e456c4c988b0be998ccf2aa30c812905e2537f
lionel <ndabagajeanlionel@gmail.com>
2024-02-04 23:18:41 +0200

    divided web app into simpler manageable parts

--- home/views.py
+++ home/views.py
@@ -2,16 +2,16 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, "index.html")
+    return render(request, "pages/index.html")
 
 
 def login(request):
-    return render(request, "login.html")
+    return render(request, "pages/auth/login.html")
 
 
 def signup(request):
-    return render(request, "signup.html")
+    return render(request, "pages/auth/signup.html")
 
 
 def products(request):
-    return render(request, "products.html")
+    return render(request, "pages/products.html")
